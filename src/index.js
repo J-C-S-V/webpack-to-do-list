60657db0c422eeefe66acda3b43c8f54eb6d402f
Juan Sanchez <juansan1993@hotmail.com>
2023-03-28 17:39:40 -0500

    Add array and function to JS file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import './index.scss';
-console.log('Hello World');
+
+const tasks = [
+  { description: 'sleep', completed: true, index: 1 },
+  { description: 'eat', completed: true, index: 2 },
+  { description: 'study', completed: true, index: 3 },
+];
+
+const ul = document.getElementsByClassName('.list');
+
+const tasksList = () => {
+  tasks.forEach((element) => {
+    // console.log(element.description);
+    const li = document.createElement('li');
+    li.innerHTML = element.description;
+    ul.appendChild(li);
+  });
+};
+
+tasksList();
