04f71f47ab5f36166aa1fd0d5be03697a6a71135
Juan Sanchez <juansan1993@hotmail.com>
2023-03-28 18:23:06 -0500

    Update styles

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ const tasks = [
   { description: 'study', completed: true, index: 3 },
 ];
 
-const ul = document.getElementsByClassName('.list');
+const ul = document.querySelector('.list');
 
 const tasksList = () => {
   tasks.forEach((element) => {
     // console.log(element.description);
     const li = document.createElement('li');
     li.innerHTML = element.description;
+    li.classList.add('ul_li');
     ul.appendChild(li);
   });
 };
