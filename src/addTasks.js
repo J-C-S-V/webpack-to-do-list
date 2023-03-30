da1e67d0e6b98b4a96c4fa64e3b8007256a9bb67
Juan Sanchez <juansan1993@hotmail.com>
2023-03-29 21:48:04 -0500

    Update event listeners to work properly

--- src/addTasks.js
+++ src/addTasks.js
@@ -1,28 +1,38 @@
 import { removeTasks } from './removeTasks.js';
 import { editTasks } from './editTasks.js';
-const tasks = [];
 
 const input = document.querySelector('.main__input');
 const ul = document.querySelector('.list');
 
 export default function addTasks() {
+  const tasks = [];
+
   tasks.push({
     description: input.value,
     completed: false,
     index: tasks.length + 1,
   });
 
+  // Add li
   const li = document.createElement('li');
-  li.innerHTML = tasks[tasks.length - 1].description;
-  li.setAttribute('id', tasks.length);
   li.classList.add('list__item');
   ul.appendChild(li);
 
+  // Add span for task description
+  const taskSpan = document.createElement('span');
+  taskSpan.classList.add('list__item-description');
+  taskSpan.innerHTML = tasks[tasks.length - 1].description;
+  li.appendChild(taskSpan);
+
+  // Add remove button
   const removeButton = document.createElement('button');
   removeButton.innerHTML = 'Remove';
   removeButton.classList.add('remove-button');
   li.appendChild(removeButton);
-  editTasks();
-  input.value = '';
+
+  // Add event listeners for remove and edit buttons
   removeButton.addEventListener('click', removeTasks);
+  editTasks(li);
+
+  input.value = '';
 }
--- src/editTasks.js
+++ src/editTasks.js
@@ -1,10 +1,8 @@
-export function editTasks() {
-  const li2 = document.querySelector('.list__item');
-  li2.addEventListener('click', () => {
-    const currentContent = li2.textContent;
-    const newContent = prompt('Edit task', currentContent);
+export function editTasks(li) {
+  const taskSpan = li.querySelector('.list__item-description');
+  taskSpan.addEventListener('click', () => {
+    const newContent = prompt('Edit task');
     if (newContent === null || newContent === '') return;
-    li2.innerHTML = `${newContent}<button class="remove-button">Remove</button>
-    `;
+    taskSpan.textContent = newContent;
   });
 }
