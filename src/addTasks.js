1b17754d75d141c0a564d50071cdc6fc7792962f
Juan Sanchez <juansan1993@hotmail.com>
2023-03-30 16:04:17 -0500

    Fix linter errors 3

--- src/addTasks.js
+++ src/addTasks.js
@@ -1,6 +1,8 @@
-import { ul, tasks, input } from './index.js';
+import { tasks, input } from './index.js';
 import removeTasks from './removeTasks.js';
 import editTasks from './editTasks.js';
+
+export const ul = document.querySelector('.list');
 
 export default function addTasks() {
   tasks.push({
