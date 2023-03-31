1c0030a09bdd3e941fe4948812c525a426a3aa41
Juan Sanchez <juansan1993@hotmail.com>
2023-03-31 12:03:17 -0500

    Fix repeated code

--- src/index.js
+++ src/index.js
@@ -1,42 +1,39 @@
 import './index.scss';
-// import isCompleted from './isCompleted.js';
 
 const addButton = document.querySelector('.main__button');
-
 const input = document.querySelector('.main__input');
 const ul = document.querySelector('.list');
 const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-function removeTasks(event) {
-  event.stopPropagation();
-  const li = event.target.parentNode;
-  const index = Array.prototype.indexOf.call(ul.children, li);
+function removeTask(event) {
+  const li = event.target.closest('li');
+  if (!li) return;
+  const index = Array.from(li.parentNode.children).indexOf(li);
   tasks.splice(index, 1);
   li.remove();
 
-  for (let i = index; i < tasks.length; i += 1) {
+  for (let i = index; i < tasks.length; i++) {
     tasks[i].index = i + 1;
   }
 
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-function editTasks(li) {
+function editTask(li) {
   const taskSpan = li.querySelector('.list__item-description');
   taskSpan.addEventListener('click', () => {
     const newContent = prompt('Edit task');
     if (newContent === null || newContent === '') return;
     taskSpan.textContent = newContent;
-    const index = Array.prototype.indexOf.call(ul.children, li);
+    const index = Array.from(li.parentNode.children).indexOf(li);
     tasks[index].description = newContent;
     localStorage.setItem('tasks', JSON.stringify(tasks));
   });
 }
 
-tasks.forEach((task) => {
+function createListItem(task) {
   const li = document.createElement('li');
   li.classList.add('list__item');
-  ul.appendChild(li);
 
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
@@ -46,50 +43,51 @@ tasks.forEach((task) => {
 
   const taskSpan = document.createElement('span');
   taskSpan.classList.add('list__item-description');
-  taskSpan.innerHTML = task.description;
+  taskSpan.textContent = task.description;
   li.appendChild(taskSpan);
 
   const removeButton = document.createElement('button');
-  removeButton.innerHTML = 'Remove';
+  removeButton.textContent = 'Remove';
   removeButton.classList.add('remove-button');
   li.appendChild(removeButton);
 
-  removeButton.addEventListener('click', removeTasks);
-  editTasks(li);
-});
+  return li;
+}
 
-function addTasks() {
-  tasks.push({
-    description: input.value,
+function renderList() {
+  ul.innerHTML = '';
+  tasks.forEach((task) => {
+    const li = createListItem(task);
+    ul.appendChild(li);
+    editTask(li);
+  });
+}
+
+function addTask() {
+  const description = input.value.trim();
+  if (!description) return;
+  const task = {
+    description,
     completed: false,
     index: tasks.length + 1,
-  });
+  };
+  tasks.push(task);
 
-  const li = document.createElement('li');
-  li.classList.add('list__item');
+  const li = createListItem(task);
   ul.appendChild(li);
-
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.classList.add('list__item-checkbox');
-  // checkbox.checked = task.completed;
-  li.appendChild(checkbox);
-
-  const taskSpan = document.createElement('span');
-  taskSpan.classList.add('list__item-description');
-  taskSpan.innerHTML = tasks[tasks.length - 1].description;
-  li.appendChild(taskSpan);
-
-  const removeButton = document.createElement('button');
-  removeButton.innerHTML = 'Remove';
-  removeButton.classList.add('remove-button');
-  li.appendChild(removeButton);
-
-  removeButton.addEventListener('click', removeTasks);
-  editTasks(li);
+  editTask(li);
 
   input.value = '';
 
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
-addButton.addEventListener('click', addTasks);
+
+ul.addEventListener('click', (event) => {
+  if (event.target.classList.contains('remove-button')) {
+    removeTask(event);
+  }
+});
+
+addButton.addEventListener('click', addTask);
+
+renderList();
