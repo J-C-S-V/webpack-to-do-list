b112531744e84db05d91af77fe15ee2be12c7e30
Juan Sanchez <juansan1993@hotmail.com>
2023-05-31 16:30:04 -0500

    Update render function

--- src/modules/renderList.js
+++ src/modules/renderList.js
@@ -1,5 +1,6 @@
 import removeTask from './removeTasks.js';
 import isCompleted from './isCompleted.js';
+import clearAllCompleted from './clearAllCompleted.js';
 
 const renderList = () => {
   const ul = document.querySelector('.main__ul');
@@ -8,17 +9,21 @@ const renderList = () => {
     const li = document.createElement('li');
     li.classList.add('list__item');
     li.innerHTML = `
-    <input type="checkbox" class="list__item-checkbox">
+    <input type="checkbox" class="list__item-checkbox" ${element.completed ? 'checked' : ''}>
     <span class="list__item-description" contenteditable="true">${element.description}</span>
     <button class="remove-button">Remove</button>
   `;
+    if (element.completed) {
+      li.classList.add('completed');
+    }
     ul.appendChild(li);
 
     const removeButton = document.querySelectorAll('.remove-button');
     removeButton.forEach((button) => button.addEventListener('click', removeTask));
-
     const checkbox = document.querySelectorAll('.list__item-checkbox');
     checkbox.forEach((box) => box.addEventListener('click', isCompleted));
+    const clearAllButton = document.querySelectorAll('.main__anchor');
+    clearAllButton.forEach((item) => item.addEventListener('click', clearAllCompleted));
   });
 };
 
