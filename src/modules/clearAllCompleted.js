5f49e3979e8c2a95cc1b5f791be7d5f9885f8c25
Juan Sanchez <juansan1993@hotmail.com>
2023-06-04 09:29:36 -0500

    Fix indexes when clearing all completed tasks

--- src/modules/clearAllCompleted.js
+++ src/modules/clearAllCompleted.js
@@ -2,14 +2,15 @@ function clearAllCompleted() {
   const tasks = (JSON.parse(localStorage.getItem('tasks'))) || [];
 
   const completedTasks = tasks.filter((task) => !task.completed);
+  completedTasks.forEach((task, index) => {
+    task.index = index;
+  });
+
   localStorage.setItem('tasks', JSON.stringify(completedTasks));
 
   const completedItems = document.querySelectorAll('.completed');
   completedItems.forEach((item) => {
     item.remove();
-  });
-  tasks.forEach((task, index) => {
-    task.index = index;
   });
 }
 
